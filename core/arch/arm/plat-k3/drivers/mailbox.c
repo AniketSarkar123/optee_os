0cf6472f62fecaee4c6bbb7581a26b7710bab531
Aniket Sarkar <a-sarkar1@ti.com>
2025-07-10 16:31:40 +0530

    Fixed issues in mailbox driver
    All the function return type issues and other inconsistencies
    are fixed. New file change in main.c is also added where
    memory mapping is done for the mailbox driver and support
    for working of all the functions is added for AM62L.
    
    Signed-off-by: Aniket Sarkar <a-sarkar1@ti.com>

--- core/arch/arm/plat-k3/drivers/mailbox.c
+++ core/arch/arm/plat-k3/drivers/mailbox.c
@@ -1,20 +1,20 @@
+// SPDX-License-Identifier: BSD-2-Clause
 /*
  * Texas Instruments K3 Mailbox Driver
  * Copyright (C) 2025 Texas Instruments Incorporated - https://www.ti.com/
  */
 
-#include <stddef.h>
-#include <string.h>
-#include <kernel/delay.h>
-#include <string.h>
 #include <io.h>
-#include <ti_sci_transport.h>
+#include <kernel/delay.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
-#include <ti_sci_protocol.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
+#include "ti_sci_protocol.h"
+#include <ti_sci_transport.h>
 
 /*
  * TI Mailbox Register Offsets
@@ -50,19 +50,21 @@ static void *mailbox_rx_sram_va;
 
 void mailbox_init(void)
 {
-	mailbox_rx_base = (void *)core_mmu_get_va(TI_MAILBOX_RX_BASE, MEM_AREA_IO_SEC, 0x1000);
-	mailbox_tx_base = (void *)core_mmu_get_va(TI_MAILBOX_TX_BASE, MEM_AREA_IO_SEC, 0x1000);
-	mailbox_tx_sram_va = (void *)core_mmu_get_va(MAILBOX_TX_START_REGION, MEM_AREA_IO_SEC, 0x1000);
-	mailbox_rx_sram_va = (void *)core_mmu_get_va(MAILBOX_RX_START_REGION, MEM_AREA_IO_SEC, 0x1000);
+	mailbox_rx_base = (void *)core_mmu_get_va(TI_MAILBOX_RX_BASE,
+	MEM_AREA_IO_SEC, 0x1000);
+	mailbox_tx_base = (void *)core_mmu_get_va(TI_MAILBOX_TX_BASE,
+	MEM_AREA_IO_SEC, 0x1000);
+	mailbox_tx_sram_va = (void *)core_mmu_get_va(MAILBOX_TX_START_REGION,
+	MEM_AREA_IO_SEC, 0x1000);
+	mailbox_rx_sram_va = (void *)core_mmu_get_va(MAILBOX_RX_START_REGION,
+	MEM_AREA_IO_SEC, 0x1000);
 
-	if (!mailbox_rx_base || !mailbox_tx_base || !mailbox_tx_sram_va || !mailbox_rx_sram_va) {
-		EMSG("Failed to map mailbox MMIO or SRAM regions: rx=%p tx=%p tx_sram=%p rx_sram=%p",
-		     mailbox_rx_base, mailbox_tx_base, mailbox_tx_sram_va, mailbox_rx_sram_va);
+	if (!mailbox_rx_base || !mailbox_tx_base ||
+	 !mailbox_tx_sram_va || !mailbox_rx_sram_va) {
+		EMSG("Failed to map mailbox MMIO or SRAM regions");
 		return;
 	}
 
-	IMSG("Mailbox MMIO and SRAM regions mapped: rx=%p tx=%p tx_sram=%p rx_sram=%p",
-	      mailbox_rx_base, mailbox_tx_base, mailbox_tx_sram_va, mailbox_rx_sram_va);
 }
 
 /*
@@ -75,13 +77,14 @@ static int8_t ti_mailbox_poll_rx_status(void)
 	uint32_t retry_count = 100U;
 
 	while (num_messages_pending == 0U) {
-		num_messages_pending = io_read32((vaddr_t)mailbox_rx_base + TI_MAILBOX_MSG_STATUS);
+		num_messages_pending = io_read32((vaddr_t)mailbox_rx_base +
+		TI_MAILBOX_MSG_STATUS);
 
 		if (retry_count-- == 0U) {
 			EMSG("Mailbox RX status polling timed out");
 			return -1;
 		}
-		udelay(10000);
+		mdelay(10);
 	}
 
 	return 0;
@@ -98,25 +101,26 @@ TEE_Result ti_sci_transport_send(const struct ti_sci_msg *msg)
 	paddr_t pa;
 
 	if (!msg)
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 
 	num_bytes = msg->len;
 
-	if (io_read32((vaddr_t)mailbox_tx_base + TI_MAILBOX_FIFO_STATUS) != 0U) {
+	if (io_read32((vaddr_t)mailbox_tx_base +
+	TI_MAILBOX_FIFO_STATUS) != 0U) {
 		EMSG("Mailbox TX FIFO is not empty");
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	if (num_bytes > MAILBOX_MAX_MESSAGE_SIZE) {
 		EMSG("Message size exceeds maximum allowed size");
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	memmove(dst_ptr, msg->buf, num_bytes);
 	pa = virt_to_phys(dst_ptr);
 	io_write32((vaddr_t)mailbox_tx_base + TI_MAILBOX_MSG, (uint32_t)pa);
 
-	return 0;
+	return TEE_SUCCESS;
 }
 
 /*
@@ -132,61 +136,60 @@ TEE_Result ti_sci_transport_recv(const struct ti_sci_msg *msg)
 	uint8_t *rcv_va;
 
 	if (!msg)
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 
 	num_bytes = msg->len / sizeof(uint8_t);
 
 	if (ti_mailbox_poll_rx_status() != 0) {
 		EMSG("Mailbox RX status polling failed");
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	rcv_addr = io_read32((vaddr_t)mailbox_rx_base + TI_MAILBOX_MSG);
 	snprintf(hex_str, sizeof(hex_str), "0x%lx", rcv_addr);
-	IMSG("Hex value of rcv_addr is: %s", hex_str);
 
 	val = (uint32_t)strtoul(hex_str, NULL, 0);
 
 	if (val < MAILBOX_RX_START_REGION) {
 		EMSG("Message address %lu is not valid\n", rcv_addr);
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	if (num_bytes > MAILBOX_MAX_MESSAGE_SIZE) {
 		EMSG("Message length %lu > max msg size\n", rcv_addr);
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	rcv_va = (uint8_t *)core_mmu_get_va(rcv_addr, MEM_AREA_IO_SEC, 0x1000);
 	if (!rcv_va) {
 		EMSG("Failed to get virtual address for RX");
-		return -1;
+		return TEE_ERROR_BAD_STATE;
 	}
 
 	memmove(msg->buf, (uint8_t *)rcv_va, num_bytes);
-	return 0;
+	return TEE_SUCCESS;
 }
 
 /*
  * ti_sci_clear_init() - Clear the mailbox RX status and initialize the mailbox
  */
 
-uint32_t ti_sci_clear_init(void)
+TEE_Result ti_sci_clear_init(void)
 {
 	uint32_t try_count;
 
 	mailbox_init();
 
 	try_count = io_read32((vaddr_t)mailbox_rx_base + TI_MAILBOX_MSG_STATUS);
-	while (io_read32((vaddr_t)mailbox_rx_base + TI_MAILBOX_MSG_STATUS) != 0U) {
+	while (io_read32((vaddr_t)mailbox_rx_base +
+	TI_MAILBOX_MSG_STATUS) != 0U) {
 		io_read32((vaddr_t)mailbox_rx_base + TI_MAILBOX_MSG);
 		if (try_count == 0U) {
 			EMSG("Mailbox RX status polling timed out");
-			return -1;
+			return TEE_ERROR_BAD_STATE;
 		}
 		try_count--;
 	}
 
-	IMSG("Mailbox RX status cleared after %u tries", try_count);
-	return 0;
+	return TEE_SUCCESS;
 }
